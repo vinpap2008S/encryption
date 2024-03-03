1e217d6340e1d75b3b39a2644a965fc6ce0a4e7d
vinpap2008S <mirzoevsergej881@gmail.com>
2024-03-03 19:17:14 +0300

    шифрование

--- main.py
+++ main.py
@@ -1,81 +1,59 @@
-import string
+year = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4:30,
+    5:31,
+    6:30,
+    7:31,
+    8:31,
+    9:30,
+    10:31,
+    11:30,
+    12:31
+}
+up_year = {
+    1: 31,
+    2: 29,
+    3: 31,
+    4:30,
+    5:31,
+    6:30,
+    7:31,
+    8:31,
+    9:30,
+    10:31,
+    11:30,
+    12:31
+}
+one_mas = []
+two_mas = []
+for f in open("a0.txt"):
+    count = 0
+    one, two = f.split("\t")
+    one_mas = list(map(int,one.split(".")))
+    two_mas = list(map(int,two.split(".")))
+    for i in range(one_mas[-1], two_mas[-1]):
+        count += 366 if i % 4 == 0 else 365
+    if one_mas[2] % 4 == two_mas[2] % 4:
+        count -= 1
 
-def enigma_encrypt(message, rotors, reflector):
-    message = message.upper()
-    encrypted_message = ""
-    for char in message:
-        if char in string.ascii_uppercase:
-            index = (ord(char) - ord('A')) % 26
-            for rotor in rotors:
-                index = (index + rotor) % 26
-            index = (index + reflector) % 26
-            for rotor in reversed(rotors):
-                index = (index - rotor) % 26
-            encrypted_char = chr(index + ord('A'))
-            encrypted_message += encrypted_char
-            # Поворот роторов после каждого символа
-            rotors[0] = (rotors[0] + 1) % 26
-            if rotors[0] == 0:
-                rotors[1] = (rotors[1] + 1) % 26
-                if rotors[1] == 0:
-                    rotors[2] = (rotors[2] + 1) % 26
-                    if rotors[2] == 0:
-                        rotors[3] = (rotors[3] + 1) % 26
-                        if rotors[3] == 0:
-                            rotors[4] = (rotors[4] + 1) % 26
-        else:
-            encrypted_message += char
-    return encrypted_message
-
-def enigma_decrypt(message, rotors, reflector):
-    decrypted_message = ""
-    for char in message:
-        if char in string.ascii_uppercase:
-            index = (ord(char) - ord('A')) % 26
-            for rotor in rotors:
-                index = (index + rotor) % 26
-            index = (index + reflector) % 26
-            for rotor in reversed(rotors):
-                index = (index - rotor) % 26
-            decrypted_char = chr(index + ord('A'))
-            decrypted_message += decrypted_char
-            # Поворот роторов после каждого символа
-            rotors[0] = (rotors[0] + 1) % 26
-            if rotors[0] == 0:
-                rotors[1] = (rotors[1] + 1) % 26
-                if rotors[1] == 0:
-                    rotors[2] = (rotors[2] + 1) % 26
-                    if rotors[2] == 0:
-                        rotors[3] = (rotors[3] + 1) % 26
-                        if rotors[3] == 0:
-                            rotors[4] = (rotors[4] + 1) % 26
-        else:
-            decrypted_message += char
-    return decrypted_message
-
-# Пример использования:
-rotors = [1, 2, 3, 4, 5]  # Позиции роторов (0-25)
-reflector = 6       # Позиция рефлектора (0-25)
-
-alpha = 'abcdefghijklmnopqrstuvwxyz'
-n = int(input())
-s = input().strip()
-res = ''
-for c in s:
-    res += alpha[(alpha.index(c) + n) % len(alpha)]
-print('Result: "' + res + '"')
-
-message = res
-encrypted_message = enigma_encrypt(message, rotors, reflector)
-print("Зашифрованное сообщение:", encrypted_message)
+    while not(one_mas[0] == two_mas[0] and one_mas[1] == two_mas[1]):
+        one_mas[0] = one_mas[0] + 1
+        if up_year[one_mas[1]] < one_mas[0] and one_mas[2] % 4 == 0:
+            one_mas[0] = 1
+            one_mas[1] = one_mas[1] + 1
+            if one_mas[1] > 12:
+                one_mas[1] = 1
+                one_mas[2] = one_mas[2] + 1
+        if year[one_mas[1]] < one_mas[0] and one_mas[2] % 4 != 0:
+            one_mas[0] = 1
+            one_mas[1] = one_mas[1] + 1
+            if one_mas[1] > 12:
+                one_mas[1] = 1
+                one_mas[2] = one_mas[2] + 1
+        count += 1
 
 
+    print(count)
 
-decrypted_message = enigma_decrypt(encrypted_message, rotors, -reflector)
-print("Расшифрованное сообщение:", decrypted_message)
-
-decrypted_message = decrypted_message.lower()
-res1 = ""
-for c in decrypted_message:
-    res1 += alpha[(alpha.index(c) - n) % len(alpha)]
-print(res1)
